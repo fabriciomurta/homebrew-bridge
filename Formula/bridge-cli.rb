602aaa2c983fb3f1f1ba730a1885d4bd06a6eff3
Fabrício Zimmerer Murta <fabricio@object.net>
2017-12-05 17:55:17 -0200

    Cleans up brew audit complaints and add '--HEAD' support.

--- Formula/bridge-cli.rb
+++ Formula/bridge-cli.rb
@@ -1,26 +1,23 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class BridgeCli < Formula
   desc "Bridge.NET CLI"
   homepage "https://bridge.net/"
   url "https://github.com/bridgedotnet/CLI.git", :tag => "v0.1-alpha"
   version "0.1-alpha"
 
-  #devel do
-  #  url "https://github.com/bridgedotnet/CLI.git", :branch => "master"
-  #end
+  # Currently, the development branch is 'master'.
+  head "https://github.com/bridgedotnet/CLI.git", :branch => "master"
 
-  # if tar/gz is desired
-  #url "https://github.com/bridgedotnet/CLI/tarball/v0.1-alpha"
-  #sha256 "010b8456d1fbec98cbbbebba07509124799d23f3823931f956bfd3fc0247cb8a"
+  # devel do
+  #  url "https://github.com/bridgedotnet/CLI.git", :branch => "master"
+  # end
+
+  # #if tar/gz is desired
+  # url "https://github.com/bridgedotnet/CLI/tarball/v0.1-alpha"
+  # sha256 "010b8456d1fbec98cbbbebba07509124799d23f3823931f956bfd3fc0247cb8a"
 
   depends_on "mono" => :run
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
     system "xbuild", "/p:Configuration=Release", "Bridge.CLI.sln"
 
     Dir.chdir("Bridge/bin/Release") do
